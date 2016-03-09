c411b6dc237688324ef437480d4f2fca024ec650
mueed1991 <mueed.ch@outlook.com>
2016-03-09 19:57:33 +0500

    check-in

---
+++ lazy-loaders/get_datas2.php
@@ -0,0 +1,272 @@
+﻿
+
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/7/unsplash-2c0343f7-2.jpg" class="img-responsive">
+            <h3>Article 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/8/flickr-3959602354-6d3e62cbf2-o.jpg" class="img-responsive">
+            <h3>Article 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/32/stocksnap-d322d377ad.jpg" class="img-responsive">
+            <h3>Article 3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/36/2933505325-f570a10466-o.jpg" class="img-responsive">
+            <h3>Article 4</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/39/stocksnap-vlkldh5k98.jpg" class="img-responsive">
+            <h3>Article 5</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/45/stocksnap-p0b6lapfpc.jpg" class="img-responsive">
+            <h3>Article 6</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/47/img-0286.jpg" class="img-responsive">
+            <h3>Article 7</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/54/9544486445-6bb7c38fb2-z.jpg" class="img-responsive">
+            <h3>Article 8</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/65/stocksnap-l3l1v89nlf.jpg" class="img-responsive">
+            <h3>Article 9</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/5/stocksnap-qa2ui38djp.jpg" class="img-responsive">
+            <h3>Article 10</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/9/flickr-5532175591-831a09741f-o.jpg" class="img-responsive">
+            <h3>Article 11</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/22/110222817-3aca7ba866-o.jpg" class="img-responsive">
+            <h3>Article 12</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/30/stocksnap-4d10ac38b0.jpg" class="img-responsive">
+            <h3>Article 13</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/40/stocksnap-5ayvzqia71.jpg" class="img-responsive">
+            <h3>Article 14</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/41/stocksnap-4fd7de07b0.jpg" class="img-responsive">
+            <h3>Article 15</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/49/img-0284.jpg" class="img-responsive">
+            <h3>Article 16</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/51/4319474846-37c77b4b89-z.jpg" class="img-responsive">
+            <h3>Article 17</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/61/stocksnap-f21c81c492.jpg" class="img-responsive">
+            <h3>Article 18</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/4/flickr-16655983955-7f7c5d2383-k.jpg" class="img-responsive">
+            <h3>Article 19</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/10/flickr-854006082-e44cd82437-o.jpg" class="img-responsive">
+            <h3>Article 20</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/18/2870728868-654a16112f-o.jpg" class="img-responsive">
+            <h3>Article 21</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/34/stocksnap-a535fdbb21.jpg" class="img-responsive">
+            <h3>Article 22</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/42/flickr-16463311698-e52678e6c7-k.jpg" class="img-responsive">
+            <h3>Article 23</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/46/stocksnap-l3l1v89nlf.jpg" class="img-responsive">
+            <h3>Article 24</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/48/img-0281.jpg" class="img-responsive">
+            <h3>Article 25</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/52/7500228392-e9557d2cb3-z.jpg" class="img-responsive">
+            <h3>Article 26</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/59/stocksnap-vl28zdfjzy.jpg" class="img-responsive">
+            <h3>Article 27</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/11/flickr-5535305608-e292b3d1b4-o.jpg" class="img-responsive">
+            <h3>Article 28</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/19/3565927533-37d166655b-o.jpg" class="img-responsive">
+            <h3>Article 29</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
+    <article class="col-sm-12 lazyItem">
+        <div class="lazyContent">
+            <img alt="" src="http://cms.pandao.eu/medias/article/small/31/picjumbo-com-20140220-dsc-0147.jpg" class="img-responsive">
+            <h3>Article 30</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis nisl sed dolor fermentum porta. Cras quis lorem tincidunt, commodo magna sed, accumsan nunc.</p>
+            <p><span class="label label-default">Posted: 2016-03-09</span><span class="label label-default">By: pandao</span></p>
+            <div class="clearfix"></div>
+        </div>
+    </article>
